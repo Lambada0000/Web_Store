e8b4c3919af18cecc3155258f8612fdfc91737ae
георгий савинцев <igsgo4645@gmail.com>
2024-07-05 23:20:55 +0400

    created models - Product and Category

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,78 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(
+        max_length=50,
+        verbose_name="Наименование категории",
+        help_text="Введите наименование категории",
+    )
+    description = models.TextField(
+        max_length=200,
+        verbose_name="Описание категории",
+        help_text="Введите описание категории",
+        blank=True,
+        null=True,
+    )
+
+    class Meta:
+        verbose_name = "Категория"
+        verbose_name_plural = "Категории"
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    name = models.CharField(
+        max_length=50,
+        verbose_name="Наименование продукта",
+        help_text="Введите наименование продукта",
+    )
+    description = models.TextField(
+        max_length=200,
+        verbose_name="Описание продукта",
+        help_text="Введите описание продукта",
+        blank=True,
+        null=True,
+    )
+    image = models.ImageField(
+        upload_to="product/photo",
+        blank=True,
+        null=True,
+        verbose_name="Изображение продукта",
+        help_text="Загрузите фото продукта",
+    )
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.SET_NULL,
+        verbose_name="Категория",
+        help_text="Введите категорию",
+        null=True,
+        blank=True,
+        related_name="categories",
+    )
+    price = models.IntegerField(
+        blank=True,
+        null=True,
+        verbose_name="Цена", help_text="Укажите цену за покупку"
+    )
+    created_at = models.DateField(
+        blank=True,
+        null=True,
+        verbose_name="Дата создания записи",
+        help_text="Укажите дату создания записи",
+    )
+    updated_at = models.DateField(
+        blank=True,
+        null=True,
+        verbose_name="Дата последнего изменения записи",
+        help_text="Укажите дату последнего изменения записи",
+    )
+
+    class Meta:
+        verbose_name = "Продукт"
+        verbose_name_plural = "Продукты"
+
+    def __str__(self):
+        return self.name
