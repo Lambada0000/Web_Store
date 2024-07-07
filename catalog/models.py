0a52cf912bdc0964e671a0b4c62b6ddbfba15e1d
георгий савинцев <igsgo4645@gmail.com>
2024-07-07 11:37:20 +0400

    add admin.py and create dumpdata

--- catalog/models.py
+++ catalog/models.py
@@ -68,12 +68,6 @@ class Product(models.Model):
         verbose_name="Дата последнего изменения записи",
         help_text="Укажите дату последнего изменения записи",
     )
-    manufactured_at = models.DateField(
-        blank=True,
-        null=True,
-        verbose_name="Дата производства продукта",
-        help_text="Укажите дату производства продукта",
-    )
 
     class Meta:
         verbose_name = "Продукт"
