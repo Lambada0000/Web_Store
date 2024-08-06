2025c7e71a042a34fa74fba0dbc5218d0d473f71
георгий савинцев <igsgo4645@gmail.com>
2024-08-06 11:24:31 +0400

    fix app blog

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from blog.models import Blog
 
-# Register your models here.
+
+@admin.register(Blog)
+class BlogAdmin(admin.ModelAdmin):
+    list_display = ('name', 'slug', 'description', 'photo', 'created_at', 'is_published', 'views')
+    search_fields = ('name', 'is_published')
+
+
+def my_media(data):
+    if data:
+        return f'/media/product/photo/{data}'
+    return f'/media/product/photo/empty_picture.jpg'
--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
-from blog.views import index, BlogCreateView
 from blog.apps import BlogConfig
+from blog.views import BlogCreateView, BlogListView, BlogDetailView, BlogUpdateView, BlogDeleteView
 
 app_name = BlogConfig.name
 
 urlpatterns = [
-    path('', index, BlogCreateView.as_view(), name='blog_create')
+    path('', BlogListView.as_view(), name='list'),
+    path('blog/<int:pk>/', BlogDetailView.as_view(), name='blog_detail'),
+    path('create/', BlogCreateView.as_view(), name='create'),
+    path('blog/<int:pk>/update', BlogUpdateView.as_view(), name='blog_update'),
+    path('blog/<int:pk>/delete/', BlogDeleteView.as_view(), name='blog_delete'),
 ]
