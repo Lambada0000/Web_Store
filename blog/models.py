bd6961b28170397e4e3eb0914d668201417fb276
георгий савинцев <igsgo4645@gmail.com>
2024-08-09 01:50:59 +0400

    fix default photo

--- blog/models.py
+++ blog/models.py
@@ -20,7 +20,7 @@ class Blog(models.Model):
     )
     photo = models.ImageField(
         upload_to="product/photo",
-        default='media/product/photo/default.jpg',
+        default='product/photo/default.jpg',
         blank=True,
         null=True,
         verbose_name="Изображение",
