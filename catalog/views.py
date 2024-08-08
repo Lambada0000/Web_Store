015362cb20f7cc8bc30ba759c1ca37a94b30863f
георгий савинцев <igsgo4645@gmail.com>
2024-08-09 01:20:41 +0400

    add views count and success url

--- catalog/views.py
+++ catalog/views.py
@@ -1,4 +1,4 @@
-from django.urls import reverse_lazy
+from django.urls import reverse_lazy, reverse
 from django.views.generic import ListView, DetailView, DeleteView, UpdateView
 from catalog.models import Product
 
@@ -10,13 +10,22 @@ class ProductListView(ListView):
 class ProductDetailView(DetailView):
     model = Product
 
-
-class ProductDeleteView(DeleteView):
-    model = Product
-    success_url = reverse_lazy('catalog:product_list')
+    def get_object(self, queryset=None):
+        self.object = super().get_object(queryset)
+        self.object.views += 1
+        self.object.save()
+        return self.object
 
 
 class ProductUpdateView(UpdateView):
     model = Product
     fields = ('name', 'description', 'price')
+    success_url = reverse_lazy('catalog:product_list')
+
+    def get_success_url(self):
+        return reverse('blog:blog_detail', args=[self.kwargs.get('pk')])
+
+
+class ProductDeleteView(DeleteView):
+    model = Product
     success_url = reverse_lazy('catalog:product_list')
