8c578f1b2bc3e75ae75f146841fef5fdb889b731
георгий савинцев <igsgo4645@gmail.com>
2024-08-09 14:07:41 +0400

    fix

--- blog/urls.py
+++ blog/urls.py
@@ -6,8 +6,8 @@ app_name = BlogConfig.name
 
 urlpatterns = [
     path('', BlogListView.as_view(), name='list'),
-    path('blog/<int:pk>/', BlogDetailView.as_view(), name='blog_detail'),
+    path('blog/<int:pk>/', BlogDetailView.as_view(), name='detail'),
     path('create/', BlogCreateView.as_view(), name='create'),
-    path('blog/<int:pk>/update', BlogUpdateView.as_view(), name='blog_update'),
-    path('blog/<int:pk>/delete/', BlogDeleteView.as_view(), name='blog_delete'),
+    path('blog/<int:pk>/update', BlogUpdateView.as_view(), name='update'),
+    path('blog/<int:pk>/delete/', BlogDeleteView.as_view(), name='delete'),
 ]
