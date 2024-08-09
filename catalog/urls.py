c4e7e6e041c5df348920f91b5a18f78c84fe3a4b
георгий савинцев <igsgo4645@gmail.com>
2024-08-09 14:06:14 +0400

    add createview

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
 from catalog.apps import CatalogConfig
-from catalog.views import ProductListView, ProductDetailView, ProductDeleteView, ProductUpdateView
+from catalog.views import ProductListView, ProductDetailView, ProductDeleteView, ProductUpdateView, ProductCreateView
 
 app_name = CatalogConfig.name
 
 urlpatterns = [
     path('', ProductListView.as_view(), name='product_list'),
     path('product/<int:pk>/', ProductDetailView.as_view(), name='product_detail'),
+    path('create/', ProductCreateView.as_view(), name='product_create'),
     path('product/<int:pk>/delete/', ProductDeleteView.as_view(), name='product_delete'),
     path('product/<int:pk>/update/', ProductUpdateView.as_view(), name='product_update')
 ]
