b8d5c9ef6580c2381094349226304daf7c806d78
георгий савинцев <igsgo4645@gmail.com>
2024-08-10 21:42:26 +0400

    add MixinForm

--- catalog/forms.py
+++ catalog/forms.py
@@ -1,14 +1,24 @@
-from django.forms import ModelForm
+from django.forms import ModelForm, BooleanField
 from .models import Product, Version
 
 
-class ProductForm(ModelForm):
+class StyleFormMixin:
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            if isinstance(field, BooleanField):
+                field.widget.attrs["class"] = "form-check-input"
+            else:
+                field.widget.attrs["class"] = "form-control"
+
+
+class ProductForm(StyleFormMixin, ModelForm):
     class Meta:
         model = Product
         fields = ('name', 'description', 'photo', 'category', 'price')
 
 
-class VersionForm(ModelForm):
+class VersionForm(StyleFormMixin, ModelForm):
     class Meta:
         model = Version
         fields = "__all__"
