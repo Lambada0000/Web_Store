a144bd12fcfeb7646e77d17d94244587fde9fade
георгий савинцев <igsgo4645@gmail.com>
2024-08-10 21:30:35 +0400

    add versions

--- catalog/forms.py
+++ catalog/forms.py
@@ -1,8 +1,14 @@
 from django.forms import ModelForm
-from .models import Product
+from .models import Product, Version
 
 
 class ProductForm(ModelForm):
     class Meta:
         model = Product
         fields = ('name', 'description', 'photo', 'category', 'price')
+
+
+class VersionForm(ModelForm):
+    class Meta:
+        model = Version
+        fields = "__all__"
