22acace50093f33b9dbff5a0aeff842728a3994f
георгий савинцев <igsgo4645@gmail.com>
2024-08-10 19:57:17 +0400

    create ProductForm

--- catalog/forms.py
+++ catalog/forms.py
@@ -1,10 +1,8 @@
 from django.forms import ModelForm
-
-from catalog.models import Product
+from .models import Product
 
 
 class ProductForm(ModelForm):
     class Meta:
         model = Product
-        fields = "__all__"
-
+        fields = ('name', 'description', 'photo', 'category', 'price')
