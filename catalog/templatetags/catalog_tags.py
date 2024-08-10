5de9df92d05b026a02a0d46a158b3fa66f1ef9cb
георгий савинцев <igsgo4645@gmail.com>
2024-08-10 19:08:28 +0400

    add default photo in monitor

--- catalog/templatetags/catalog_tags.py
+++ catalog/templatetags/catalog_tags.py
@@ -5,7 +5,10 @@ register = template.Library()
 
 
 @register.filter()
-def media_filter(path):
-    if path:
-        return f"{settings.MEDIA_URL}{path}"
-    return f"{settings.MEDIA_URL}product/photo/default/default_notebook.png"
+def media_filter(photo, product):
+    if photo:
+        return f"{settings.MEDIA_URL}{photo}"
+    if product.category.pk == 1:
+        return f"{settings.MEDIA_URL}product/photo/default/default_notebook.png"
+    elif product.category.pk == 2:
+        return f"{settings.MEDIA_URL}product/photo/default/default_monitor.png"
