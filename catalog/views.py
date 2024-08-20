8e3fd0a9ab0b5684f377f9e961adbad7556ef332
георгий савинцев <igsgo4645@gmail.com>
2024-08-20 13:31:10 +0400

    make method get_form_class

--- catalog/views.py
+++ catalog/views.py
@@ -1,9 +1,16 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.core.exceptions import PermissionDenied
 from django.forms import inlineformset_factory
 from django.urls import reverse_lazy, reverse
 from django.utils.text import slugify
-from django.views.generic import ListView, DetailView, DeleteView, UpdateView, CreateView
-from .forms import ProductForm, VersionForm
+from django.views.generic import (
+    ListView,
+    DetailView,
+    DeleteView,
+    UpdateView,
+    CreateView,
+)
+from .forms import ProductForm, VersionForm, ProductModeratorForm
 from .models import Product, Version
 
 
@@ -24,7 +31,7 @@ class ProductDetailView(DetailView):
 class ProductCreateView(CreateView, LoginRequiredMixin):
     model = Product
     form_class = ProductForm
-    success_url = reverse_lazy('catalog:product_list')
+    success_url = reverse_lazy("catalog:product_list")
 
     def form_valid(self, form):
         if form.is_valid():
@@ -39,35 +46,51 @@ class ProductCreateView(CreateView, LoginRequiredMixin):
         return super().form_valid(form)
 
 
-class ProductUpdateView(UpdateView):
+class ProductUpdateView(LoginRequiredMixin, UpdateView):
     model = Product
     form_class = ProductForm
-    success_url = reverse_lazy('catalog:product_list')
+    success_url = reverse_lazy("catalog:product_list")
 
     def get_success_url(self):
-        return reverse('catalog:product_list')
+        return reverse("catalog:product_list")
 
     def get_context_data(self, **kwargs):
         context_data = super().get_context_data(**kwargs)
         ProductFormset = inlineformset_factory(Product, Version, VersionForm, extra=1)
-        if self.request.method == 'POST':
-            context_data['formset'] = ProductFormset(self.request.POST, instance=self.object)
+        if self.request.method == "POST":
+            context_data["formset"] = ProductFormset(
+                self.request.POST, instance=self.object
+            )
         else:
-            context_data['formset'] = ProductFormset(instance=self.object)
+            context_data["formset"] = ProductFormset(instance=self.object)
         return context_data
 
     def form_valid(self, form):
         context_data = self.get_context_data()
-        formset = context_data['formset']
+        formset = context_data["formset"]
         if form.is_valid() and formset.is_valid():
             self.object = form.save()
             formset.instance = self.object
             formset.save()
             return super().form_valid(form)
         else:
-            return self.render_to_response(self.get_context_data(form=form, formset=formset))
+            return self.render_to_response(
+                self.get_context_data(form=form, formset=formset)
+            )
+
+    def get_form_class(self):
+        user = self.request.user
+        if user == self.object.creator:
+            return ProductForm
+        if (
+            user.has_perm("catalog.can_edit_product_description")
+            and user.has_perm("catalog.can_edit_product_category")
+            and user.has_perm("catalog.can_cancel_publication")
+        ):
+            return ProductModeratorForm
+        raise PermissionDenied
 
 
 class ProductDeleteView(DeleteView):
     model = Product
-    success_url = reverse_lazy('catalog:product_list')
+    success_url = reverse_lazy("catalog:product_list")
